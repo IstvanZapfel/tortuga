c5acd5e119f3050179646b08c4248451eff5a463
IstvanZapfel <105928213+IstvanZapfel@users.noreply.github.com>
2024-10-19 19:19:24 +0200

    Add files via upload

---
+++ files/sendmail/sendmail.php
@@ -0,0 +1,66 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+	require 'phpmailer/src/SMTP.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('en', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+
+	$mail->isSMTP();                                            //Send using SMTP
+	$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'istvan.mbox@gmail.com';                     //SMTP username
+	$mail->Password   = 'sitnphjwjmiqrinp';                               //SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+	$mail->Port       = 587;                 
+
+
+	//Від кого лист
+	$mail->setFrom('istvan.mbox@gmail.com', 'Istvan Capfel'); // Вказати потрібний E-mail
+	//Кому відправити
+	$mail->addAddress('thorns@ukr.net'); // Вказати потрібний E-mail
+	//Тема листа
+	$mail->Subject = 'E-mail from test';
+
+	//Тіло листа
+	$body = '<h1>Hi! It`s Test!</h1>';
+
+	//if(trim(!empty($_POST['email']))){
+		//$body.=$_POST['email'];
+	//}	
+	
+	/*
+	//Прикріпити файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//шлях завантаження файлу
+		$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
+		//грузимо файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото у додатку</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+	*/
+
+	$mail->Body = $body;
+
+	//Відправляємо
+	if (!$mail->send()) {
+		$message = 'Помилка';
+	} else {
+		$message = 'Дані надіслані!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
